5e8fb98f616963e9c10bd50a2d3729d9e6611f49
Suryaprakash17 <70808230+Suryaprakash17@users.noreply.github.com>
2020-10-17 22:31:27 +0530

    Day4

---
+++ Oddvalues.java
@@ -0,0 +1,21 @@
+package Oddvalues;
+
+public class Oddvalues {
+
+	public static void main(String[] args) {
+		
+		int a[]= {1,2,3,4,5};
+		
+		for(int i=0;i<a.length;i++) {
+			
+			if(a[i]%2!=0) {
+				
+				System.out.println(a[i]);
+				
+			}
+			
+		}
+
+	}
+
+}
